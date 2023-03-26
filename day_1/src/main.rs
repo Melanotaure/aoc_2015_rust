89a386efdc1ed4fe229e2e6ea40243ea7dfab749
unknown <luc.gaborit@gmail.com>
2023-03-26 15:41:30 +0200

    First part solution

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = String::from("puzzle_input.txt");
+    let file = read_to_string(&filename).expect("Unable to read puzzle file.");
+
+    let floor = file.chars().fold(0, |acc, c| acc + match c { '(' => 1, ')' => -1, _ => 0, });
+
+    println!("Part One:");
+    println!("  Floor: {floor}");
+
+    println!("\nPart Two:");
 }
