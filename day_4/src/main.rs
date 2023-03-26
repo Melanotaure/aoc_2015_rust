e6ac40303f1b09102c06dc9760caae503c6e759a
unknown <luc.gaborit@gmail.com>
2023-03-26 19:46:14 +0200

    Day 4 part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -3,7 +3,7 @@ use md5;
 const SECRET_KEY: &str = "iwrupvqb";
 
 fn main() {
-    let mut counter = 0;
+    let mut counter: i64 = 0;
 
     loop {
         let advent_coin = SECRET_KEY.to_string() + format!("{counter}").as_str();
@@ -17,5 +17,19 @@ fn main() {
     }
 
     println!("Part One:");
+    println!("  value: {}", counter);
+
+    println!("\nPart Two:");
+    counter = 0;
+    loop {
+        let advent_coin = SECRET_KEY.to_string() + format!("{counter}").as_str();
+        let digest = md5::compute(advent_coin.clone());
+
+        let digest = format!("{:x}", digest);
+        if digest.starts_with("000000") {
+            break;
+        }
+        counter += 1;
+    }
     println!("  value: {}", counter);
 }
