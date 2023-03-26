b43347e9a3116db1fcc60858c97a6d75b327c88c
unknown <luc.gaborit@gmail.com>
2023-03-26 16:09:58 +0200

    Added Day 1 part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -2,12 +2,25 @@ use std::fs::read_to_string;
 
 fn main() {
     let filename = String::from("puzzle_input.txt");
-    let file = read_to_string(&filename).expect("Unable to read puzzle file.");
-
-    let floor = file.chars().fold(0, |acc, c| acc + match c { '(' => 1, ')' => -1, _ => 0, });
+    let input = read_to_string(&filename).expect("Unable to read puzzle file.");
 
     println!("Part One:");
+    let floor = input.chars().fold(0, |acc, c| acc + match c { '(' => 1, ')' => -1, _ => 0, });
     println!("  Floor: {floor}");
 
     println!("\nPart Two:");
+    let mut acc = 0;
+    let mut position = 0;
+    for (pos, c) in input.chars().enumerate() {
+        acc += match c {
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
+        };
+        if acc == -1 {
+            position = pos + 1;
+            break;
+        }
+    }
+    println!("  Enter basement at: {position}");
 }
