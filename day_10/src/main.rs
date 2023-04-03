77cc06dd7f19722370713ae82a0cb6804eaf6417
Luc <luc@kickmaker.net>
2023-04-03 15:55:06 +0200

    day 10 part 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let input = String::from("3113322113");
     let mut char_vec: Vec<_> = input.chars().collect();
 
-    for _ in 0..40 {
+    for nb_times in 0..50 {
         let mut cur_vec = Vec::new();
         let mut count = 1;
         for c in char_vec.windows(2) {
@@ -16,8 +16,13 @@ fn main() {
         cur_vec.extend(count.to_string().chars());
         cur_vec.push(*char_vec.last().unwrap());
         char_vec = cur_vec;
+
+        if nb_times == 39 {
+            println!("Part One: [for 40 iterations]");
+            println!("  Result length: {}", char_vec.len());
+        }
     }
 
-    println!("Part One:");
+    println!("Part Two: [for 50 iterations]");
     println!("  Result length: {}", char_vec.len());
 }
