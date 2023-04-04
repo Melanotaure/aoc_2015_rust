88a392ecbfcdf0ced9a55fa12f61e6e7e7cbe6d0
unknown <luc.gaborit@gmail.com>
2023-04-04 18:58:18 +0200

    day 12 part 2

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,6 +1,5 @@
+use serde_json::{Map, Value};
 use std::fs::read_to_string;
-use serde_json::Value;
-
 
 fn main() {
     let filename = String::from("puzzle_input.txt");
@@ -9,14 +8,24 @@ fn main() {
     let my_json: Value = serde_json::from_str(&input).unwrap();
 
     println!("Part One:");
-    println!("  Total: {}", json_parse(0, &my_json));
+    println!("  Total: {}", json_parse(0, &my_json, true));
+    println!("Part Two: [without 'red' entries]");
+    println!("  Total: {}", json_parse(0, &my_json, false));
 }
 
-fn json_parse(accu: i64, json: &Value) -> i64 {
+fn json_parse(accu: i64, json: &Value, count_red: bool) -> i64 {
     match json {
         Value::Number(num) => accu + num.as_i64().unwrap(),
-        Value::Array(ref a) => a.iter().fold(accu, |sum, val| json_parse(sum, val)),
-        Value::Object(ref obj) => obj.values().fold(accu, |sum, val| json_parse(sum, val)),
+        Value::Array(ref a) => a
+            .iter()
+            .fold(accu, |sum, val| json_parse(sum, val, count_red)),
+        Value::Object(ref obj) if count_red || !has_red(obj) => obj
+            .values()
+            .fold(accu, |sum, val| json_parse(sum, val, count_red)),
         _ => accu,
     }
+}
+
+fn has_red(obj: &Map<String, Value>) -> bool {
+    obj.values().any(|v| *v == Value::String("red".to_string()))
 }
