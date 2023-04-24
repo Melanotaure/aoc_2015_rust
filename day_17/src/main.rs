71f738d71dfe10d559f0728155e005b70d4713ff
Luc <luc@kickmaker.net>
2023-04-24 23:13:20 +0200

    day 17 part 2

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{cmp::Ordering, fs::read_to_string};
 
 fn main() {
     let filename = String::from("puzzle_input.txt");
@@ -6,19 +6,33 @@ fn main() {
 
     let containers: Vec<_> = input.lines().map(|v| v.parse::<u32>().unwrap()).collect();
 
-    let mut combi_count = 0;
+    let mut combo_count = 0;
+    let mut min_container_number = std::u32::MAX;
+    let mut min_combo_count = 0;
     for configuration in 0..=2u32.pow(containers.len() as u32) {
         let mut acc = 0;
+        let mut container_number = 0;
         for (i, c) in containers.iter().enumerate() {
             if (configuration >> i) & 1 == 1 {
                 acc += c;
+                container_number += 1;
             }
         }
         if acc == 150 {
-            combi_count += 1;
+            combo_count += 1;
+            match container_number.cmp(&min_container_number) {
+                Ordering::Less => {
+                    min_container_number = container_number;
+                    min_combo_count = 1;
+                }
+                Ordering::Equal => min_combo_count += 1,
+                Ordering::Greater => {}
+            }
         }
     }
 
     println!("Part One:");
-    println!("Nb combinations: {}", combi_count);
+    println!("Nb combinations: {}", combo_count);
+    println!("Part Two:");
+    println!("Nb combinations: {}", min_combo_count);
 }
