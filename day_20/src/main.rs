05866e74e0b8240a51afbd54ad9ab4b48d767e25
Luc <luc@kickmaker.net>
2023-04-25 23:33:02 +0200

    day 20 refacto for part 2

--- day_20/src/main.rs
+++ day_20/src/main.rs
@@ -1,8 +1,16 @@
-const NB_GIFTS: u32 = 36000000;
+const NB_GIFTS: usize = 36000000;
 
 fn main() {
-    let goal = (NB_GIFTS / 10) as usize;
+    let goal = NB_GIFTS / 10;
     let max: usize = goal - 1;
+
+    println!("Part One:");
+    println!("Lowest house number: {}", part_one(goal, max));
+    println!("Part Two:");
+    println!("Lowest house number: {}", part_two(NB_GIFTS, max));
+}
+
+fn part_one(goal: usize, max: usize) -> usize {
     let mut houses = Vec::new();
     houses.resize(max, 1);
 
@@ -12,14 +20,27 @@ fn main() {
         }
     }
 
-    let mut saved_house_num = 0;
     for (house_num, house) in houses.iter().enumerate().take(max).skip(2) {
         if *house >= goal {
-            saved_house_num = house_num;
-            break;
+            return house_num;
         }
     }
+    unreachable!()
+}
 
-    println!("Part One:");
-    println!("Lowest house number: {}", saved_house_num);
+fn part_two(goal: usize, max: usize) -> usize {
+    let mut houses = Vec::new();
+    houses.resize(max, 1);
+
+    for elf_num in 2..max {
+        for house_num in (elf_num..max).step_by(elf_num).take(50) {
+            houses[house_num] += elf_num * 11;
+        }
+    }
+    for (house_num, house) in houses.iter().enumerate().take(max).skip(2) {
+        if *house >= goal {
+            return house_num;
+        }
+    }
+    unreachable!()
 }
